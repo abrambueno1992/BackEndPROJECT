fea3919093a354caed29be8f64f0b2e3484f0004
Abraham Bueno <abueno@localhost.localdomain>
2018-11-19 18:31:59 -0600

    Added Higher order component withAuth to check for authentication prior to rendering component.

--- card-project/src/components/PresentationMain.js
+++ card-project/src/components/PresentationMain.js
@@ -1,117 +1,118 @@
-import React from 'react';
-import { logoutAction, reorderState, getNotesAction } from '../actions/actions';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom'
-import Presentation from './Presentation';
-
-
-
+import React from "react";
+import { logoutAction, reorderState, getNotesAction } from "../actions/actions";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Presentation from "./Presentation";
+import withAuth from "../lib/withAuth";
 
 const mainSt = {
-	marginLeft: '2%',
-	paddingTop: 50
+  marginLeft: "2%",
+  paddingTop: 50
 };
 
 export class PresentationMain extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			notes: []
-		}
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: []
+    };
+  }
+  componentWillUpdate = nextProps => {
+    if (this.props.notes !== nextProps.notes) {
+      this.setState({ notes: nextProps.notes });
+    }
+    if (this.props.reOrder === true) {
+      this.setState({ notes: this.props.notes });
+    }
+    if (!this.props.notes && !nextProps.notes) {
+      this.props.getNotesAction(this.props.history);
+    }
+    if (
+      this.props.error !== nextProps.error &&
+      nextProps.error === "TokenExpiredError"
+    ) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      localStorage.removeItem("ID");
+      localStorage.removeItem("notes");
+      this.props.history.push("/");
+    }
+  };
 
-	}
-	componentWillUpdate = (nextProps) => {
-		if (this.props.notes !== nextProps.notes) {
-			this.setState({ notes: nextProps.notes });
-		}
-		if (this.props.reOrder === true) {
-			this.setState({ notes: this.props.notes })
-		}
-		if (!this.props.notes && !nextProps.notes) {
-			this.props.getNotesAction(this.props.history)
-		}
-		if (this.props.error !== nextProps.error && nextProps.error === 'TokenExpiredError') {
-			localStorage.removeItem('token');
-			localStorage.removeItem('username');
-			localStorage.removeItem('ID');
-			localStorage.removeItem('notes');
-			this.props.history.push('/');
-		}
+  componentDidMount() {
+    // if (
+    //   localStorage.getItem("token") !== null &&
+    //   localStorage.getItem("ID") !== null
+    // ) {
+    //   this.setState({ list: this.props.notes });
+    // } else {
+    //   this.setState({ autheticated: "Not authenticated, Access Denied" });
+    //   this.props.history.push("/");
+    // }
+  }
 
-	}
+  refresh = () => {
+    this.props.notes;
+    // this.forceUpdate()
+  };
 
+  toggle = e => {
+    e.preventDefault();
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+  handleDelete = id => {
+    let IDnote = {
+      Id: id
+    };
+    this.props.deleteNote(IDnote, this.props.history);
+    this.refresh();
+    this.setState({
+      modal: !this.state.modal
+    });
+    this.props.history.push("/notes");
+  };
+  handleInputChange = (checkBoolean, id) => {
+    // event.preventDefault();
+    let trueObj = {
+      check: true,
+      Id: id
+    };
+    let falseObj = {
+      check: false,
+      Id: id
+    };
+    // this.setState({checked: this.state.checked === false ? true : false});
+    // this.setState({ccolor: this.state.ccolor === 'red' ? 'blue' : 'red' });
+    checkBoolean === false
+      ? this.props.updateNote(trueObj, this.props.history)
+      : this.props.updateNote(falseObj, this.props.history);
+    // this.setState({Complete: this.state.Complete === 'NOT COMPLETE' ? 'COMPLETED' : 'NOT COMPLETE'});
+    // this.props.getNotesAction();
+    this.props.history.push("/notes");
+    // this.props.checkUpdate(this.state.checked, this.props.id);
+    // window.location.reload()
+  };
 
-
-	componentDidMount() {
-		if (localStorage.getItem('token') !== null && localStorage.getItem('ID') !== null) {
-
-			this.setState({ list: this.props.notes })
-		} else {
-			this.setState({ autheticated: 'Not authenticated, Access Denied' });
-			this.props.history.push('/')
-		}
-
-	}
-
-
-	refresh = () => {
-		this.props.notes;
-		// this.forceUpdate()
-	};
-
-	toggle = (e) => {
-		e.preventDefault();
-		this.setState({
-			modal: !this.state.modal
-		});
-	}
-	handleDelete = (id) => {
-		let IDnote = {
-			'Id': id
-		}
-		this.props.deleteNote(IDnote, this.props.history);
-		this.refresh();
-		this.setState({
-			modal: !this.state.modal
-		});
-		this.props.history.push('/notes')
-	};
-	handleInputChange = (checkBoolean, id) => {
-		// event.preventDefault();
-		let trueObj = {
-			'check': true,
-			'Id': id
-		};
-		let falseObj = {
-			'check': false,
-			'Id': id
-		}
-		// this.setState({checked: this.state.checked === false ? true : false});
-		// this.setState({ccolor: this.state.ccolor === 'red' ? 'blue' : 'red' });
-		checkBoolean === false ? this.props.updateNote(trueObj, this.props.history) : this.props.updateNote(falseObj, this.props.history);
-		// this.setState({Complete: this.state.Complete === 'NOT COMPLETE' ? 'COMPLETED' : 'NOT COMPLETE'});
-		// this.props.getNotesAction();
-		this.props.history.push('/notes')
-		// this.props.checkUpdate(this.state.checked, this.props.id);
-		// window.location.reload() 
-
-
-	}
-
-	render() {
-
-		return (
-			<div style={mainSt}>
-				<Presentation ndata={this.props.notes} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div style={mainSt}>
+        <Presentation ndata={this.props.notes} />
+      </div>
+    );
+  }
 }
-const mapDispatchToProps = (state) => {
-	return {
-		notes: state.notes,
-		error: state.error
-	};
+const mapDispatchToProps = state => {
+  return {
+    notes: state.notes,
+    error: state.error
+  };
 };
 
-export default withRouter(connect(mapDispatchToProps, { logoutAction, getNotesAction, reorderState })(PresentationMain));
+export default withRouter(
+  connect(
+    mapDispatchToProps,
+    { logoutAction, getNotesAction, reorderState }
+  )(withAuth(PresentationMain))
+);
---
+++ card-project/src/lib/withAuth.js
@@ -0,0 +1,72 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+let globalUser = null;
+
+export default (Page, { loginRequired = true, logoutRequired = false } = {}) =>
+  class BaseComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        authenticated:
+          localStorage.getItem("token") !== null &&
+          localStorage.getItem("ID") !== null
+            ? true
+            : false,
+        checked: false
+      };
+    }
+    componentDidMount() {
+      //Get credentials from storage, cookies, or wherever
+      //Check credentials if any
+      //if there are no credentials redirect to login
+      //if there are credentials, give permission to render Page (aka the component)
+      //   if (
+      //     localStorage.getItem("token") !== null &&
+      //     localStorage.getItem("ID") !== null
+      //   ) {
+      //     this.setState({ list: this.props.notes });
+      //   } else {
+      //     this.setState({ autheticated: "Not authenticated, Access Denied" });
+      //     this.props.history.push("/");
+      //   }
+      //   if (loginRequired && !logoutRequired && !user) {
+      //     Router.push("/public/login", "/login");
+      //     this.props.history.push("/notes");
+      //     return;
+      //   }
+      //   if (logoutRequired && user) {
+      //     Router.push("/");
+      //   }
+      //   if (adminRequired && (!user || !user.isAdmin)) {
+      //     Router.push('/customer/my-books', '/my-books');
+      //   }
+      if (this.state.authenticated === true) {
+        this.props.history.push("/notes");
+        console.log("true, props are", this.props);
+      }
+      if (this.state.authenticated === false) {
+        this.props.history.push("/login");
+        console.log("false, props are false", this.props);
+      }
+    }
+
+    render() {
+      //   const { user } = this.props;
+
+      //   if (loginRequired && !logoutRequired && !user) {
+      //     return null;
+      //   }
+
+      //   if (logoutRequired && user) {
+      //     return null;
+      //   }
+
+      //   if (adminRequired && (!user || !user.isAdmin)) {
+      //     return null;
+      //   }
+
+      return <Page {...this.props} />;
+    }
+  };
