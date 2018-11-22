88896d4b37953c101a1aaaaaba04656b1b7207d6
Abraham Bueno <abueno@localhost.localdomain>
2018-11-22 13:06:48 -0600

    Played around with HOC withAuth. Removed lock files and modules, then reinstalled; to fix security issue with lock files.

--- card-project/src/lib/withAuth.js
+++ card-project/src/lib/withAuth.js
@@ -1,10 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
-let globalUser = null;
-
-export default (Page, { loginRequired = true, logoutRequired = false } = {}) =>
+export default Page =>
   class BaseComponent extends React.Component {
     constructor(props) {
       super(props);
@@ -18,54 +14,15 @@ export default (Page, { loginRequired = true, logoutRequired = false } = {}) =>
       };
     }
     componentDidMount() {
-      //Get credentials from storage, cookies, or wherever
-      //Check credentials if any
-      //if there are no credentials redirect to login
-      //if there are credentials, give permission to render Page (aka the component)
-      //   if (
-      //     localStorage.getItem("token") !== null &&
-      //     localStorage.getItem("ID") !== null
-      //   ) {
-      //     this.setState({ list: this.props.notes });
-      //   } else {
-      //     this.setState({ autheticated: "Not authenticated, Access Denied" });
-      //     this.props.history.push("/");
-      //   }
-      //   if (loginRequired && !logoutRequired && !user) {
-      //     Router.push("/public/login", "/login");
-      //     this.props.history.push("/notes");
-      //     return;
-      //   }
-      //   if (logoutRequired && user) {
-      //     Router.push("/");
-      //   }
-      //   if (adminRequired && (!user || !user.isAdmin)) {
-      //     Router.push('/customer/my-books', '/my-books');
-      //   }
       if (this.state.authenticated === true) {
-        // this.props.history.push("/notes");
         console.log("true, props are", this.props);
       }
       if (this.state.authenticated === false) {
         this.props.history.push("/login");
-        console.log("false, props are false", this.props);
       }
     }
 
     render() {
-      //   const { user } = this.props;
-
-      //   if (loginRequired && !logoutRequired && !user) {
-      //     return null;
-      //   }
-
-      //   if (logoutRequired && user) {
-      //     return null;
-      //   }
-
-      //   if (adminRequired && (!user || !user.isAdmin)) {
-      //     return null;
-      //   }
       if (this.state.authenticated) {
         return <Page {...this.props} />;
       } else {
